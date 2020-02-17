c1ed0d4cbef7e1f77b1546959bdb96db19e3ab5b
Ngethe <ciru930@gmail.com>
2020-02-17 12:45:01 +0300

    added the function to render an error page in the errors.py file

--- app/__init__.py
+++ app/__init__.py
@@ -2,34 +2,50 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_simplemde import SimpleMDE
 
 
+
+# Instances of flask extensions
+# Instance of LoginManger and using its methods
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+simple = SimpleMDE()
 
 def create_app(config_name):
+    '''
+    Function that takes configuration setting key as an argument
+    
+    Args:
+        config_name : name of the configuration to be used
+    '''
 
+    # Initialising application
     app = Flask(__name__)
 
+    simple.init_app(app)
+
     # Creating the app configurations
-
     app.config.from_object(config_options[config_name])
-    config_options[config_name].init_app(app)
 
-    # Initializing flask extensions
+
+    # Initialising flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
-
-    # Registering the blueprint
+    # Regestering the main blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # setting config
-    from .requests import configure_request
-    configure_request(app)
+    # Regestering the auth bluprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-
-
+    
 
     return app
